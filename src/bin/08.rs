f6aa3a6f691ae19ad8730ae51eb9be44e1ec807e
Sindre Bakken Næsset <sindrebn@exorlive.com>
2025-12-08 20:07:32 +0100

    formatting

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,7 +1,6 @@
-use std::collections::HashSet;
-use std::vec;
 use advent_of_code::IVec3;
 use itertools::Itertools;
+use std::collections::HashSet;
 
 advent_of_code::solution!(8);
 
@@ -19,7 +18,7 @@ pub fn parse_input(input: &str) -> Vec<IVec3> {
         .collect()
 }
 
-pub fn collect_sorted_distances(boxes: &Vec<IVec3>, count: usize) -> Vec<(IVec3, IVec3)> {
+pub fn collect_sorted_distances(boxes: &[IVec3], count: usize) -> Vec<(IVec3, IVec3)> {
     boxes
         .iter()
         .tuple_combinations()
@@ -42,24 +41,24 @@ pub fn count_circuits(input: &str, count: usize) -> Option<u64> {
                 (true, false) => {
                     circuit.insert(b);
                     continue 'outer;
-                },
+                }
                 (false, true) => {
                     circuit.insert(a);
                     continue 'outer;
-                },
+                }
                 (false, false) => (),
             }
         }
-        circuits.push(vec![a, b].iter().copied().collect());
+        circuits.push([a, b].iter().copied().collect());
     }
 
-    while circuits
-        .iter()
-        .tuple_combinations()
-        .any(|(a, b)| a.iter().any(|item| b.contains(item)) || b.iter().any(|item| a.contains(item))) {
+    while circuits.iter().tuple_combinations().any(|(a, b)| {
+        a.iter().any(|item| b.contains(item)) || b.iter().any(|item| a.contains(item))
+    }) {
         for (i, j) in (0..circuits.len()).tuple_combinations() {
             if circuits[i].iter().any(|b| circuits[j].contains(b))
-                || circuits[j].iter().any(|b| circuits[i].contains(b)) {
+                || circuits[j].iter().any(|b| circuits[i].contains(b))
+            {
                 for b in circuits[j].clone() {
                     circuits[i].insert(b);
                 }
@@ -71,7 +70,8 @@ pub fn count_circuits(input: &str, count: usize) -> Option<u64> {
     let counts: Vec<u64> = circuits
         .iter()
         .map(|c| c.len() as u64)
-        .sorted_by(|a, b| b.cmp(a)).take(3)
+        .sorted_by(|a, b| b.cmp(a))
+        .take(3)
         .collect();
 
     Some(counts.iter().product::<u64>())
@@ -81,7 +81,7 @@ pub fn part_one(input: &str) -> Option<u64> {
     count_circuits(input, 1000)
 }
 
-pub fn collect_sorted_pairs_p2(boxes: &Vec<IVec3>) -> Vec<(IVec3, IVec3)> {
+pub fn collect_sorted_pairs_p2(boxes: &[IVec3]) -> Vec<(IVec3, IVec3)> {
     boxes
         .iter()
         .tuple_combinations()
@@ -104,7 +104,7 @@ pub fn part_two(input: &str) -> Option<u64> {
             .map(|(i, _)| i)
             .collect();
         if matching_circuits.is_empty() {
-            circuits.push(vec![a, b].iter().copied().collect());
+            circuits.push([a, b].iter().copied().collect());
             continue;
         }
         let first_match = *matching_circuits.first().unwrap();
@@ -118,7 +118,7 @@ pub fn part_two(input: &str) -> Option<u64> {
             circuits.remove(idx);
         }
         if circuits.iter().any(|c| c.len() == boxes.len()) {
-            return Some(a.x as u64 * b.x as u64)
+            return Some(a.x as u64 * b.x as u64);
         }
     }
 
